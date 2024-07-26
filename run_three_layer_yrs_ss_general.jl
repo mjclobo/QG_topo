97f3ac6d1259795df856c6d30b4dfd9db4ac65bf
mjclobo <mattlobo@princeton.edu>
2024-07-26 13:57:39 -0400

    add linear option in file name

--- run_three_layer_yrs_ss_general.jl
+++ run_three_layer_yrs_ss_general.jl
@@ -191,12 +191,18 @@ while ss_yr_cnt < ss_yr_max
 
             # saving yearly output
             println("Saving annual data for year: "*string(yr_cnt))
-            if topo_type=="y_slope"
-                jld_name = data_dir*"/threelayer_h0"* string(round(h0*Lx,digits=9))*"_kt"* string(Int(kt)) *"_res" * string(Int(Nx)) * "_yr"*string(yr_cnt)*  ".jld"
-            elseif topo_type=="sin_sin"
-                jld_name = data_dir*"/threelayer_h0"* string(Int(h0[1]))*"_kt"* string(Int(kt[1])) *"_res" * string(Int(Nx)) *"_yr"*string(yr_cnt)*  ".jld"  
+            if linear==true
+                lin_str = "_linear_"
             else
-                jld_name = data_dir*"/threelayer_h0"* string(Int(h0))*"_kt"* string(Int(kt)) *"_res" * string(Int(Nx)) *"_yr"*string(yr_cnt)*  ".jld"
+                lin_str = "_"
+            end
+
+            if topo_type=="y_slope"
+                jld_name = data_dir*"/threelayer_h0"* string(round(h0*Lx,digits=9))*"_kt"* string(Int(kt)) * lin_str * "res" * string(Int(Nx)) * "_yr"*string(yr_cnt)*  ".jld"
+            elseif topo_type=="sin_sin"
+                jld_name = data_dir*"/threelayer_h0"* string(Int(h0[1]))*"_kt"* string(Int(kt[1])) * lin_str * "_res" * string(Int(Nx)) *"_yr"*string(yr_cnt)*  ".jld"  
+            else
+                jld_name = data_dir*"/threelayer_h0"* string(Int(h0))*"_kt"* string(Int(kt)) * lin_str * "_res" * string(Int(Nx)) *"_yr"*string(yr_cnt)*  ".jld"
             end
 
             println("Saving output data to JLD to: "*jld_name)
