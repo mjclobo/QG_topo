a5f1db7a39eaef92e8198c409be2535b58f7c8b4
mjclobo <mattlobo@princeton.edu>
2025-04-07 20:29:05 -0400

    changing scope of prob sub params after resetting nu

--- run_three_layer_yrs_ss_general.jl
+++ run_three_layer_yrs_ss_general.jl
@@ -151,9 +151,10 @@ while yr_cnt < ss_yr_max
 
     ##########################
     if dyn_nu==true
-        println("Changing nu")
         rmsζ = maximum(abs.(irfft(-grid.Krsq .* prob.vars.ψh[:,:,1], grid.ny)))  # sqrt(mean((irfft(-grid.Krsq .* prob.vars.ψh[:,:,1], grid.ny)).^2))
         global prob = @set prob.params.ν = 10 * rmsζ * grid.dx^8
+        global sol, clock, params, vars, grid = prob.sol, prob.clock, prob.params, prob.vars, prob.grid
+        x, y = grid.x, grid.y
     end
     
     stepforward!(prob)
