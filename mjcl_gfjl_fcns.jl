7595a48f1957529ccba3d26dbc416a1d059d694a
mjclobo <mattlobo@princeton.edu>
2025-07-21 20:59:21 -0400

    accounting for two possible names for restart file

--- mjcl_gfjl_fcns.jl
+++ mjcl_gfjl_fcns.jl
@@ -318,7 +318,11 @@ function set_initial_conditions(prob, prob_filt, model_params)
 
             a = load(pre_multilayer_dir * r_file_name)
 
-            ψ = a["jld_data"]["psi_ot"][:,:,:,1]
+            if @isdefined(a["jld_data"]["psi_ot"])
+                ψ = a["jld_data"]["psi_ot"][:,:,:,end]
+            else
+                ψ = a["jld_data"]["psi_yrs_end"][:,:,:]
+            end
 
             MultiLayerQG.set_ψ!(prob.sol, prob.params, prob.vars, prob.grid, ψ)   # this also sets q!!!
 
